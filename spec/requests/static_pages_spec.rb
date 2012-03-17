9386d13451677358e85a3fd31f4991aad19e2aac
web developer #1 <gitwebdev1@yahoo.com>
2012-03-16 22:22:57 -0700

    Fill in layout and start signup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,39 +1,30 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+  subject { page }
 
   describe "Home page" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_content('Sample App')
-    end
-    it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-              :text => "Home | #{base_title}")
-    end
+    before {visit root_path}
+    it { should have_content('Sample App') }
+    it { should have_selector('title',
+            :text => full_title("Home")) }
   end
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_content('Help')
-    end
-    it "should have the right title" do
-      visit '/static_pages/help'
-      page.should have_selector('title',
-              :text => "Help | #{base_title}")
-    end
+    before {visit help_path}
+    it { should have_content('Help') }
+    it { should have_selector('title',
+            :text => full_title("Help")) }
   end
   describe "About page" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_content('About Us')
-    end
-    it "should have the right title" do
-      visit '/static_pages/about'
-      page.should have_selector('title',
-              :text => "About | #{base_title}")
-    end
+    before {visit about_path}
+    it { should have_content('About Us') }
+    it { should have_selector('title',
+            :text => full_title("About")) }
+  end
+  describe "Contact page" do
+    before {visit contact_path}
+    it { should have_content('Contact') }
+    it { should have_selector('title',
+            :text => full_title("Contact")) }
   end
 end

87d8e70af4b4fbc9b41736b317952b4cf399bea4
web developer #1 <gitwebdev1@yahoo.com>
2012-03-20 22:05:03 -0700

    Signup Ch 7 exercise solutions

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,11 @@
 module UsersHelper
 
   # Returns the Gravatar (http://gravatar.com/) for the given user.
-  def gravatar_for(user)
+  def gravatar_for(user, opts={})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    opts[:size] = "#{opts[:size]}x#{opts[:size]}" if opts && opts.has_key?(:size)
+    image_tag gravatar_url, {alt: user.name, class: "gravatar"}.merge(opts)
+          #( size == nil ? {} : {size: "#{size}x#{size}"} ) )
   end
 end

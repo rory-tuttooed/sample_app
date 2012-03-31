5f20b3a0e796632469490cde891eb893923548b0
web developer #1 <gitwebdev1@yahoo.com>
2012-03-30 18:10:32 -0700

    user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
+
   before_save :create_remember_token
 
   validates :name, presence: true, length: {maximum: 50}
@@ -22,6 +24,10 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: valid_email_regex },
                     uniqueness: { case_sensitive: false }
   validates :password, length: {minimum: 6}
+
+  def feed
+    Micropost.where('user_id = ?', id)
+  end
 
   private
     def create_remember_token

8ff594922eb0c02e641e5eed3ae5405fe51420a0
web developer #1 <gitwebdev1@yahoo.com>
2012-04-02 15:54:35 -0700

    Following users, standard buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 SampleApp::Application.routes.draw do
 
-resources :users
+# resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
 resources :sessions, only: [:new, :create, :destroy]
 resources :microposts, only: [:create, :destroy]
+resources :relationships, only: [:create, :destroy]
 
 match '/signin', to: 'sessions#new'
 match '/signout', to: 'sessions#destroy', via: :delete
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -22,6 +22,19 @@ describe "Static pages" do
           page.should have_selector("tr##{item.id}", text: item.content)
         end
       end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before { user.follow!(other_user) }
+
+        it { should have_selector('a', href: following_user_path(user),
+                                       content: "0 following") }
+        it { should have_selector('a', href: followers_user_path(user), 
+                                       content: "1 follower") }
+#        it { should have_link('1 follower') }
+#        it { should have_link('Home', href: home_path) }
+      end
+
     end
   end
   describe "Help page" do
